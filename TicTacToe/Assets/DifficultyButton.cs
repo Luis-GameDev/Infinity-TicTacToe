808893aa35e310792af304fbfd0bfaa93ea4ea9a
LuisThiermann <luis.ljate@gmail.com>
2025-01-13 15:45:17 +0100

    UI/UX improvements

--- TicTacToe/Assets/DifficultyButton.cs
+++ TicTacToe/Assets/DifficultyButton.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using static GameManager;
+using UnityEngine.UI;
 
 public class DifficultyButton : MonoBehaviour
 {
     [SerializeField] private GameManager gm;
     [SerializeField] private TextMeshProUGUI buttonText;
     private Difficulty[] difficulties = { Difficulty.easy, Difficulty.medium, Difficulty.hard };
-    
+
     private void Start()
     {
         UpdateButtonText();
@@ -25,6 +26,19 @@ public class DifficultyButton : MonoBehaviour
 
     private void UpdateButtonText()
     {
+        Image parentImage = buttonText.GetComponentInParent<Image>();
+        switch (gm.currentDifficulty)
+        {
+            case Difficulty.easy:
+            parentImage.color = Color.green;
+            break;
+            case Difficulty.medium:
+            parentImage.color = new Color(1.0f, 0.64f, 0.0f); // Orange
+            break;
+            case Difficulty.hard:
+            parentImage.color = Color.red;
+            break;
+        }
         buttonText.text = gm.currentDifficulty.ToString();
     }
     
